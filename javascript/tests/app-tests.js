5da6665194ddbcbdf379363cb414313c557e3841
SethThomaspowered <athomas.seth@gmail.com>
2022-03-25 01:21:33 -0400

    changed name

--- javascript/tests/app-tests.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe("Greet", () =>{
-    it('returns "Welcome to Lights Out"', ()=>{
-        const expected="Welcome to Lights Out";
-        const result = Greet()
-        assert.strictEqual(expected, result);
-    })
-})
