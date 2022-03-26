85b7ab8866597d5407e1ad297f0f77d5111bd80c
Seth Thomas <athomas.seth@gmail.com>
2022-03-26 16:56:39 -0400

    create gameboard

--- javascript/app.js
+++ javascript/app.js
@@ -8,4 +8,25 @@ class Menu  {
 }
 
 }
+const init = () =>{
+    console.log('Welcome to Lights Out');
+    const gameBoard = document.querySelector('#board');
+    generateBoard(gameBoard);
+}
+const generateBoard = (board, rows = 6, columns=5) =>{
+    for (let row = 0; row<rows; row++) {
+        const elmRow = document.createElement('ul');
+        elmRow.setAttribute('data-row', row);
+        
+        for (let column = 0; column< columns; column++){
+            const elmColumn =document.createElement('li');
+            elmColumn.setAttribute('data-status', 'empty');
+            elmColumn.setAttribute('data-animation', 'idle');
+            elmColumn.textContent= ":)";
+            elmRow.appendChild(elmColumn);
+        } 
+        board.appendChild(elmRow);
+    }
+}
+document.addEventListener('DOMContentLoaded', init);
 module.exports = Menu;
