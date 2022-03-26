cfcc604e38dec9ab213ca1866ba6449364ad185b
SethThomaspowered <athomas.seth@gmail.com>
2022-03-26 00:29:23 -0400

    add menu class

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+class Menu  {
+    constructor(message, name){
+        this.message=message
+        this.name=name;
+    }
+    greet() {
+    return "Welcome to Lights Out";
+}
+
+}
+module.exports = Menu;
