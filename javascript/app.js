502f2fa804e63aee68eb9ed8efc9400b5849c291
SethThomaspowered <athomas.seth@gmail.com>
2022-03-27 00:50:00 -0400

    light switch from on to off

--- javascript/app.js
+++ javascript/app.js
@@ -10,10 +10,18 @@ class Menu  {
 }
 const init = () =>{
     console.log('Welcome to Lights Out');
+    const mainTitle = document.querySelector("#title");
     const gameBoard = document.querySelector('#board');
+    generateHeader(mainTitle);
     generateBoard(gameBoard);
 }
-const generateBoard = (board, rows = 6, columns=5) =>{
+const generateHeader = (header) =>{
+    const title = document.createElement('h1');
+    title.textContent="Welcome to Lights Out";
+    header.appendChild(title);
+
+}
+const generateBoard = (board, rows = 5, columns=5) =>{
     for (let row = 0; row<rows; row++) {
         const elmRow = document.createElement('ul');
         elmRow.setAttribute('data-row', row);
@@ -22,10 +30,27 @@ const generateBoard = (board, rows = 6, columns=5) =>{
             const elmColumn =document.createElement('li');
             elmColumn.setAttribute('data-status', 'empty');
             elmColumn.setAttribute('data-animation', 'idle');
-            elmColumn.textContent= ":)";
+            elmColumn.className='white';
+            elmColumn.textContent="__";
+           
             elmRow.appendChild(elmColumn);
+            elmColumn.addEventListener('click', (el)=>{
+                toggleLights(elmColumn)
+            })
         } 
         board.appendChild(elmRow);
+    }
+}
+const toggleLights=(element)=>{
+    if (element.classList.contains('white')){
+        element.classList.remove('white');
+        element.classList.add("black");
+
+       console.log('background is white');
+    }else{
+        element.classList.remove('black');
+        element.classList.add('white');
+        console.log("background is not white");
     }
 }
 document.addEventListener('DOMContentLoaded', init);
