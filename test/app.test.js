e972ee91476f0500cf8d17caf0da4b3990a1f7ac
Seth Thomas <athomas.seth@gmail.com>
2022-03-27 00:48:33 -0400

    add more tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
 const Menu = require('../javascript/app')
 
-describe("Greet", () =>{
-    it('returns "Welcome to Lights Out"', ()=>{
-        const expected="Welcome to Lights Out";
-        const menu = new Menu();
-        const result = menu.greet()
-        assert.strictEqual(expected, result);
+describe("Menu", ()=>{
+    describe(".greet", () =>{
+        it('returns "Welcome to Lights Out"', ()=>{
+            const expected="Welcome to Lights Out";
+            const menu = new Menu();
+            const result = menu.greet()
+            assert.strictEqual(expected, result);
+        })
     })
+})
+
+describe('init', ()=>{
+    it('returns a welcome message', ()=>{
+        const expected= "Welcome to Lights Out";
+        assert.ok()
+    }) 
 })
