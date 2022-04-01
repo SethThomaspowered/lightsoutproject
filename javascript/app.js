d7245a4d2f56d9ca27aa8166f357dbd63deb39c5
SethThomaspowered <athomas.seth@gmail.com>
2022-04-01 01:02:17 -0400

    add start and pause buttons

--- javascript/app.js
+++ javascript/app.js
@@ -31,8 +31,7 @@ const generateBoard = (board, rows = 3, columns=3) =>{
         for (let column = 0; column< columns; column++){
             const elmColumn =document.createElement('li');
             elmColumn.setAttribute('data-column', column);
-            elmColumn.setAttribute('data-status', 'empty');
-            elmColumn.setAttribute('data-animation', 'idle');
+            elmColumn.classList.add('square');
             elmColumn.className='white';
             elmColumn.classList.add('box');
             elmColumn.classList.add(column);
@@ -186,10 +185,55 @@ const reset=()=>{
     for(let i= 0; i<boxes.length; i++){
         boxes[i].classList.add('white');
     }
+
 }
 const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click', (e)=>{
     reset();
 });
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let timerRef = document.querySelector('.timerDisplay');
+let int = null;
+
+document.getElementById('startTimer').addEventListener('click', ()=>{
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+});
+
+document.getElementById('pauseTimer').addEventListener('click', ()=>{
+    clearInterval(int);
+});
+
+document.getElementById('reset').addEventListener('click', ()=>{
+    clearInterval(int);
+    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+});
+
+function displayTimer(){
+    milliseconds+=10;
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+ let h = hours < 10 ? "0" + hours : hours;
+ let m = minutes < 10 ? "0" + minutes : minutes;
+ let s = seconds < 10 ? "0" + seconds : seconds;
+ let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+
+ timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+}
+
 
 document.addEventListener('DOMContentLoaded', init);
